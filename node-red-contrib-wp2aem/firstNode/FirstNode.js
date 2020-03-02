f48f54bf2dcae966db76a1874d593f5c1d07b6ec
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2020-03-01 20:16:33 -0800

    Added early PoC: 1) single page migration flow; 2) node-red-contrib-wp2eam module; 3) sample migration page

---
+++ node-red-contrib-wp2aem/firstNode/FirstNode.js
@@ -0,0 +1,42 @@
+const HTMLSource = require('@atjson/source-html').default
+const AemCoreCompontentsRender = require('../renders/AemCoreCompontentsRender')
+const OffsetSource = require('@atjson/offset-annotations').default
+const sanitizeHtml = require('sanitize-html')
+
+module.exports = function(RED) {
+  function firstNode(config) {
+    RED.nodes.createNode(this, config);
+
+    var context = this.context();
+    var node = this;
+
+    this.on('input', function(msg) {
+      var outMsg = { payload: {} };
+
+      // TODO: Remove
+      console.log(`HTML: ${msg.payload.content.rendered}`)
+
+      let html = sanitizeHtml(msg.payload.content.rendered, {
+        allowedTags: [ 'p', 'h1', 'h2', 'h3', 'b', 'i', 'em', 'strong', 'a' ],
+        allowedAttributes: {
+          'a': [ 'href' ]
+        },
+        allowedIframeHostnames: ['www.youtube.com']
+      });
+
+      // TODO: Remove
+      console.log(`Sanitized HTML: ${html}`)
+
+      try {
+        outMsg.payload = AemCoreCompontentsRender.render(
+          HTMLSource.fromRaw(html).convertTo(OffsetSource)
+        );
+      } catch (err) {
+        console.log(err)
+      }
+
+      node.send(outMsg);
+    });
+  }
+  RED.nodes.registerType("FirstNode", firstNode);
+};
---
+++ node-red-contrib-wp2aem/renders/AemCoreCompontentsRender.js
@@ -0,0 +1,49 @@
+const Renderer = require('@atjson/renderer-hir').default
+const classify = require('@atjson/renderer-hir').classify
+
+class AemCoreCompontentsRender extends Renderer {
+  
+  *root() {
+    return (yield)
+  }
+
+  *Paragraph() {
+    const text = (yield).join('')
+    const html = `<p>${text}</p>\n`
+    const textComponent = {
+      'jcr:createdBy': 'admin',
+        'jcr:primaryType': 'nt:unstructured',
+        'sling:resourceType': 'core/wcm/components/text/v2/text',
+        'text': html,
+        'textIsRich': true,
+    }
+    return textComponent
+  }
+
+  *Heading(heading) {
+    let text = (yield).join('')
+    let level = heading.attributes.level
+    const type = `h${level}`
+    const titleComponent  = {
+      'jcr:createdBy': 'admin',
+      'jcr:primaryType': 'nt:unstructured',
+      'sling:resourceType': 'core/wcm/components/title/v2/title',
+      'jcr:title': text,
+      'type': type,
+    }
+    return titleComponent
+  }
+
+  *Link(link) {
+    let text = (yield).join('')
+    let url = link.attributes.url
+    return `<a href="${url}">${text}</a>`
+  }
+
+  *Bold(bold) {
+    let text = (yield).join('')
+    return `<strong>${text}</strong>`
+  }
+
+}
+module.exports = AemCoreCompontentsRender
